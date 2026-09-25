ca10c9e6c7348c174815ae296e594bfc7bcbf726
babsalice-afk <babs.alice@icloud.com>
2026-09-25 17:21:57 +0200

    Refresh app cache v76

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="magische-lernwelt-v75";
+const CACHE="magische-lernwelt-v76";
 const CORE=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>Promise.allSettled(CORE.map(u=>c.add(u)))))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
