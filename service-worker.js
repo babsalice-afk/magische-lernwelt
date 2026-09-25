07d6f7ae44b3bce36d89f2159f45bc718d5cb507
babsalice-afk <babs.alice@icloud.com>
2026-09-25 19:42:45 +0200

    Refresh matching game cache v83

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="magische-lernwelt-v82";
+const CACHE="magische-lernwelt-v83";
 const CORE=["./","./index.html","./manifest.webmanifest"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>Promise.allSettled(CORE.map(u=>c.add(u)))))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
